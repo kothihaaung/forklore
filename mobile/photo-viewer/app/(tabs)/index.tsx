a8388e7d5ac64f997801df2b5d2471ac588b0bda
Thiha Aung <leothihaaung@gmail.com>
2025-07-07 18:33:38 +0700

    added safe area

--- mobile/photo-viewer/app/(tabs)/index.tsx
+++ mobile/photo-viewer/app/(tabs)/index.tsx
@@ -1,7 +1,20 @@
 import PhotoGridScreen from '@/screens/PhotoGridScreen';
+import React from "react";
+import {
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
 
 export default function HomeScreen() {
   return (
-    <PhotoGridScreen />
+    <SafeAreaView style={styles.safeArea}>
+      <PhotoGridScreen />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+  },
+});
