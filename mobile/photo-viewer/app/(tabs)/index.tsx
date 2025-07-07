68b559a58792e06d9148b9a4b0d51b0428751b8c
Thiha Aung <leothihaaung@gmail.com>
2025-07-07 18:45:39 +0700

    safe area adjustments

--- mobile/photo-viewer/app/(tabs)/index.tsx
+++ mobile/photo-viewer/app/(tabs)/index.tsx
@@ -1,9 +1,7 @@
 import PhotoGridScreen from '@/screens/PhotoGridScreen';
 import React from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-} from "react-native";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
